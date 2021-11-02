4ceaf737fe83152676bcbe0290ccd81e9674a5ba
dbaldew <dbaldew@gmail.com>
2021-11-02 11:41:57 +0100

    start

---
+++ src/main/java/nl/mijndomein/library/LibraryApplication.java
@@ -0,0 +1,23 @@
+package nl.mijndomein.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello world!";
+	}
+
+
+
+
+}
